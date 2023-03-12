307804e7d0b6cc4400839bc3f6aec831b03324f0
abdalla1611 <abdalla@post.bgu.ac.il>
2023-03-12 13:10:37 +0200

    adding a local test

--- services/node-lambda/hello.ts
+++ services/node-lambda/hello.ts
@@ -1,5 +1,3 @@
-
-import { v4 } from "uuid";
 import { S3 } from "aws-sdk";
 
 const s3Client = new S3();
@@ -7,6 +5,7 @@ const s3Client = new S3();
 async function handler(event:any, context:any){
     
     const buckets = await s3Client.listBuckets().promise()
+    console.log("bbb");
     return {
         statusCode: 200,
         body: 'here are my buckets: ' + JSON.stringify(buckets.Buckets)
