1d3142b41a166083b852a59f185ddecb0523c5ee
abdalla1611 <abdalla@post.bgu.ac.il>
2023-03-17 18:38:14 +0200

    testing the identity pool

--- test/auth/auth.test.ts
+++ test/auth/auth.test.ts
@@ -1,13 +1,28 @@
-
-
 import { AuthService } from "./AuthService";
 import { config } from "./config";
 import * as AWS from "aws-sdk";
+
+AWS.config.region = config.REGION;
+
+async function getBuckets() {
+
+    let buckets;
+    try {
+        buckets = await new AWS.S3().listBuckets().promise();
+    } catch (error) {
+        buckets = undefined;
+    }
+
+    return buckets;    
+}
 
 async function tester() {
     const authservice =  new AuthService();
     const user  = await authservice.login(config.TEST_USER_NAME, config.TEST_USER_PASSWORD);
     await authservice.GetAWSTemporaryCreds(user);
-    const creds = AWS.config.credentials; 
+    const creds = AWS.config.credentials;
+    const buckets = await getBuckets();
     console.log("debug line")
 }
+
+tester()
--- test/auth/config.ts
+++ test/auth/config.ts
@@ -3,9 +3,9 @@
 
 export const config = {
     REGION: 'us-east-2',
-    USER_POOL_ID: 'us-east-2_slyDGUdUM',
-    APP_CLIENT_ID: '3ktrnn9bv47hnfhkddnqr3291s',
-    IDENTITY_POOL_ID :'us-east-2:11eb4326-15cd-46aa-a652-cdf9b14c6e01' ,
+    USER_POOL_ID: 'us-east-2_QYnm5vzts',
+    APP_CLIENT_ID: '3vr721oji32t45l5kevmp0v6ug',
+    IDENTITY_POOL_ID :'us-east-2:6c6e0acb-3b38-4e04-b15a-f5bd53074718' ,
     TEST_USER_NAME: 'abdalla',
     TEST_USER_PASSWORD: 'abdallA123?'
 }
